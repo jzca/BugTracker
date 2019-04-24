ee7c858f335319dd1a4683eea06df18c222bf443
Jerry <contactmevia@githuborothers.com>
2019-04-24 13:33:10 -0500

    TablizeAssignTicketManagement

--- BugTracker/Models/ViewModel/Ticket/AttachmentDetailViewModel.cs
+++ BugTracker/Models/ViewModel/Ticket/AttachmentDetailViewModel.cs
@@ -13,8 +13,6 @@ namespace BugTracker.Models.ViewModel.Ticket
         public string Description { get; set; }
         public string FileUrl { get; set; }
         public DateTime DateCreated { get; set; }
-        //public virtual Ticket Tickeat { get; }
-        //public int TicketId { get; set; }
         public virtual ApplicationUser Creator { get; set; }
         public string CreatorId { get; set; }
         public bool OwnerAttachment { get; set; }
--- BugTracker/Models/ViewModel/Ticket/CommentDetailViewModel.cs
+++ BugTracker/Models/ViewModel/Ticket/CommentDetailViewModel.cs
@@ -10,8 +10,6 @@ namespace BugTracker.Models.ViewModel.Ticket
         public int Id { get; set; }
         public string Comment { get; set; }
         public DateTime DateCreated { get; set; }
-        //public virtual Ticket Ticket { get; }
-        //public int TicketId { get; set; }
         public virtual ApplicationUser Creator { get; set; }
         public string CreatorId { get; set; }
         public bool OwnerComment { get; set; }
